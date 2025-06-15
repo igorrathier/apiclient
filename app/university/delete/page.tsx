5ab7e739c3ffad66ea8dea16d59cc4c4f388ed65
igorrathier <groryyggg@gmail.com>
2025-06-15 20:55:02 -0300

    primeiro commit

---
+++ app/university/delete/page.tsx
@@ -0,0 +1,12 @@
+import DeleteUniversity from "./DeleteUniversity";
+import { Suspense } from "react";
+    
+export default function Page() {
+
+        return (
+            <Suspense>
+                <DeleteUniversity />
+            </Suspense>
+           
+        );
+}
